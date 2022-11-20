e3b3e53bdbb3f3aefabee6bf5ebcbfa53bf76ff6
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2022-11-20 23:05:20 +0100

    Updates default layout

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex gap-3">
+    <side-bar />
+    <div class="w-full mr-20">
+      <nav-bar class="mb-11" />
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
