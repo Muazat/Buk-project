6cf680c055432f064f5c531a7cc2a4e19218ed41
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2022-12-26 17:02:26 +0100

    Upgrade tailwind and resolve git conflict

--- components/Mselect.vue
+++ components/Mselect.vue
@@ -1,24 +1,16 @@
 <template>
   <div class="mb-0 w-4/5 md:w-2/3">
     <select
-      class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+      class="form-select m-0 block w-full appearance-none rounded border border-solid border-gray-300 bg-white bg-clip-padding bg-no-repeat px-3 py-1.5 text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
       aria-label="Default select example"
     >
       <option v-for="option in options" :key="option">
         {{ option }}
       </option>
     </select>
-<<<<<<< HEAD
-    
-  </div> 
-</template>
-=======
   </div>
 </template>
 
 <script setup lang="ts">
-defineProps<{
-  options: string[];
-}>();
+defineProps(["options"]);
 </script>
->>>>>>> e3ab22fdfd8a19e7fd1c73d26da38c5d032aa20e
