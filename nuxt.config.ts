8609ce01639fa3277c96284b346cfad28f3b213f
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2022-12-31 17:10:15 +0100

    Adds supabase package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   css: ["@/assets/css/main.css"],
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@nuxtjs/supabase"],
 });
