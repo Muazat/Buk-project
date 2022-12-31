fb0740db60ece04b51a08f7abb2e2eebb62bdbac
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2022-12-31 17:22:12 +0100

    Remove eslint staged

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   css: ["@/assets/css/main.css"],
   modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@nuxtjs/supabase"],
