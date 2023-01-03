f0618929cc7ea01b50efd1b163a4306bde7da317
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2023-01-03 21:59:03 +0100

    Remove debug

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,6 +1,5 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const user = useSupabaseUser();
-  console.log(user.value, to.meta.layout);
 
   if (!user.value && to.meta?.layout !== "website") {
     return navigateTo("/login");
