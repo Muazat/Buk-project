afc523c5b8ba5515e95e999f3cbd4baad61b088c
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2023-03-02 22:40:44 +0100

    Adds v-model on component

--- components/Mselect.vue
+++ components/Mselect.vue
@@ -3,6 +3,7 @@
     <select
       class="form-select m-0 block w-full appearance-none rounded border border-solid border-gray-300 bg-white bg-clip-padding bg-no-repeat px-3 py-1.5 text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
       aria-label="Default select example"
+      v-model="data"
     >
       <option v-for="option in options" :key="option">
         {{ option }}
@@ -12,5 +13,13 @@
 </template>
 
 <script setup lang="ts">
-defineProps(["options"]);
+import { useVModel } from "@vueuse/core";
+
+const props = defineProps<{
+  modelValue: string;
+  options: string[];
+}>();
+const emit = defineEmits(["update:modelValue"]);
+
+const data = useVModel(props, "modelValue", emit);
 </script>
