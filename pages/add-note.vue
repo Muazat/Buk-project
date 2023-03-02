e2d69bc99d6e89701e8d51cf472394340e4a004f
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2023-03-02 22:46:19 +0100

    Adds supabase add-note

--- pages/add-note.vue
+++ pages/add-note.vue
@@ -1,34 +1,37 @@
 <template>
-
-    <PageTitle>Add Note</PageTitle>
-    <div class="md:ml-9" ref="body">
+  <div class="md:ml-9" ref="body">
     <div class="mt-10 mb-2 font-semibold">Note Category</div>
-    <Mselect></Mselect>
-  <div class="mt-4 mb-2 font-semibold">Title</div>
-  <TextInput></TextInput>
+    <Mselect v-model="category" :options="['One', 'Two']"></Mselect>
+    <div class="mt-4 mb-2 font-semibold">Title</div>
+    <TextInput v-model="title"></TextInput>
 
-  <div class="mt-4 mb-2 font-semibold">Content</div>
-  <NoteField></NoteField>
-  <div class="mt-4 mb-2 font-semibold">Attachments</div>
-  <div class="mb-0 w-4/5 md:w-2/3">
-    <FileInput></FileInput>
+    <div class="mt-4 mb-2 font-semibold">Content</div>
+    <NoteField v-model="description"></NoteField>
+    <div class="mt-4 mb-2 font-semibold">Attachments</div>
+    <div class="mb-0 w-4/5 md:w-2/3">
+      <FileInput v-model="file"></FileInput>
+    </div>
   </div>
-</div>
-<div class="w-4/5 md:w-2/3">
-<AddButton class="bg-green-500 text-white font-semibold p-2 float-right mb-10">ADD NOTE</AddButton>
-</div>
+  <div class="w-4/5 md:w-2/3">
+    <AddButton
+      class="float-right mb-10 bg-green-500 p-2 font-semibold text-white"
+      @click="submitHandler"
+    >
+      ADD NOTE
+    </AddButton>
+  </div>
 
-<!-- work on this code to add categories to options easily -->
-<!-- <div class="hidden opacity-100 " ref="category">
+  <!-- work on this code to add categories to options easily -->
+  <!-- <div class="hidden opacity-100 " ref="category">
     <CreateCategory></CreateCategory>
 </div> -->
- <!-- <a class="text-primary md:float-right md:w-1/2" @click="categoryClickHandler">Create New Category</a> -->
-  
-  
-  
-  </template>
-  
-  <script setup lang="ts">
+  <!-- <a class="text-primary md:float-right md:w-1/2" @click="categoryClickHandler">Create New Category</a> -->
+</template>
+
+<script setup lang="ts">
+import { Database } from "~~/types/supabase";
+const supabaseClient = useSupabaseClient<Database>();
+
 // const category = ref<HTMLDivElement>(null);
 // const body = ref<HTMLDivElement>(null);
 
@@ -43,7 +46,47 @@
 //     body.value.style.opacity="1";
 // }
 
+const title = ref("");
+const description = ref("");
+const file = ref<FileList | null>(null);
+const category = ref("");
+
+const submitHandler = async () => {
+  try {
+    const { data: savedData, error } = await supabaseClient
+      .from("Notes")
+      .insert({
+        title: title.value,
+        description: description.value,
+        has_attachment: file.value ? true : false,
+        category: category.value,
+      })
+      .select("*");
+    if (error) {
+      console.log(error);
+    }
+    if (savedData) {
+      console.log(savedData, file.value);
+
+      if (file.value) {
+        const { data, error } = await supabaseClient.storage
+          .from("files")
+          .upload(
+            `${useSupabaseUser().value?.id}/notes/${
+              savedData![0].id
+            }.${file.value[0].name.split(".").at(-1)?.trim()}`,
+            file.value[0]
+          );
+        if (error) {
+          console.log(error);
+        }
+        if (data) {
+          console.log(data);
+        }
+      }
+    }
+  } catch (error) {}
+};
 </script>
-  
-  <style lang="scss" scoped></style>
-  
+
+<style lang="scss" scoped></style>
