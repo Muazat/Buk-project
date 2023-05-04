38c5bedc7728b910e3f20b3b760ca00e657e3299
Muazat Kabara <Muazat.kabara1999@gmail.com>
2023-05-03 21:27:18 -0700

    cgpa calculator

---
+++ components/CgpaAdd.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    v-if="showAdd"
+    class="mt-2 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6"
+  >
+    <!-- <div class="sm:col-span-2 sm:col-start-1">
+            <div class="mt-2">
+              <input type="text"  v-model="courseCode"
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            </div>
+          </div> -->
+
+    <div class="sm:col-span-2">
+      <div class="mt-2">
+        <input
+          type="number"
+          v-model="courseInfo.creditUnit"
+          class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        />
+      </div>
+    </div>
+
+    <div class="sm:col-span-2">
+      <div class="mt-2">
+        <Mselect :options="categoryOptions" v-model="courseInfo.grade" />
+      </div>
+    </div>
+    <button type="button" @click="showAdd = false">remove</button>
+  </div>
+</template>
+<script setup lang="ts">
+const categoryOptions = ["A", "B", "C", "D", "E", "F"];
+// const courseCode = ref("");
+const showAdd = ref(true);
+const index = defineProp("index");
+
+const courseInfo = reactive({
+  creditUnit: "",
+  grade: "",
+  showAdd,
+});
+const emit = defineEmits(["updated"]);
+watch(
+  courseInfo,
+  () => {
+    emit("updated", courseInfo, index.value, showAdd);
+  },
+  { deep: true }
+);
+</script>
---
+++ components/CgpaHeading.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mt-2 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+    <!-- <div class="sm:col-span-2 sm:col-start-1">
+      <label for="city" class="block text-sm font-medium leading-6 text-gray-900">Course code</label>
+    </div> -->
+
+    <div class="sm:col-span-2">
+      <label
+        for="region"
+        class="block text-sm font-medium leading-6 text-gray-900"
+        >Credit Unit</label
+      >
+    </div>
+
+    <div class="sm:col-span-2">
+      <label
+        for="postal-code"
+        class="block text-sm font-medium leading-6 text-gray-900"
+        >Grade</label
+      >
+    </div>
+  </div>
+</template>
+<script></script>
