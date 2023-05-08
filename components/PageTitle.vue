1fa9c0f5fc206cb24eacb46db8e74895fd4564e8
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2023-05-08 11:42:45 +0100

    Moves back button to title component

--- components/PageTitle.vue
+++ components/PageTitle.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="mt-16 md:mt-8 ml-2 md:ml-8 text-black text-2xl font-semibold">
+  <div
+    class="ml-2 mt-16 flex items-center gap-4 text-2xl font-semibold text-black md:ml-8 md:mt-8"
+  >
+    <button class="inline-flex items-center" @click="useRouter().go(-1)">
+      <Icon name="material-symbols:arrow-back" class="h-6 w-8" />
+    </button>
     <slot></slot>
   </div>
 </template>
