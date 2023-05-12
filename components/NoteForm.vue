6001a311e0ec17ea3de5127f5e3b138a54c3b38d
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2023-05-12 16:46:12 +0100

    Add note boilderplate

---
+++ components/NoteForm.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="md:ml-9" ref="body">
+    <div class="mb-2 mt-10 font-semibold">Note Category</div>
+    <Mselect
+      v-model="category"
+      :options="['Personal', 'School', 'Others']"
+    ></Mselect>
+    <div class="mb-2 mt-4 font-semibold">Title</div>
+    <TextInput v-model="title"></TextInput>
+
+    <div class="mb-2 mt-4 font-semibold">Content</div>
+    <NoteField v-model="description"></NoteField>
+    <div class="mb-2 mt-4 font-semibold">Attachments</div>
+    <div class="mb-0 w-4/5 md:w-2/3">
+      <FileInput v-model="file"></FileInput>
+    </div>
+  </div>
+  <div class="w-4/5 md:w-2/3">
+    <AddButton
+      class="float-right mb-10 bg-green-500 p-2 font-semibold text-white"
+      @click="submitHandler"
+    >
+      ADD NOTE
+    </AddButton>
+    <!-- work on this code to add categories to options easily -->
+    <!-- <div class="hidden opacity-100 " ref="category">
+        <CreateCategory></CreateCategory>
+    </div> -->
+    <!-- <a class="text-primary md:float-right md:w-1/2" @click="categoryClickHandler">Create New Category</a> -->
+    <div :class="{ 'mb-2 mt-4 font-semibold text-red-500': isErr }">
+      {{ msg }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Database } from "~~/types/supabase";
+const supabaseClient = useSupabaseClient<Database>();
+
+const title = ref("");
+const description = ref("");
+const file = ref<FileList | null>(null);
+const category = ref("");
+const msg = ref("");
+const isErr = ref(false);
+
+const submitHandler = async () => {
+  if (title.value != "" && description.value != "") {
+    try {
+      const file_ext = file.value
+        ? file.value[0].name.split(".").at(-1)?.trim()
+        : null;
+      const { data: savedData, error } = await supabaseClient
+        .from("Notes")
+        .insert({
+          title: title.value,
+          description: description.value,
+          has_attachment: file.value ? true : false,
+          category: category.value,
+          file_ext,
+        })
+        .select("*");
+      if (error) {
+        console.log(error);
+      }
+      if (savedData) {
+        if (file.value) {
+          const { data, error } = await supabaseClient.storage
+            .from("files")
+            .upload(
+              `${useSupabaseUser().value?.id}/notes/${
+                savedData[0].id
+              }.${file.value[0].name.split(".").at(-1)?.trim()}`,
+              file.value[0]
+            );
+          if (error) {
+            console.log(error);
+          }
+          if (data) {
+            console.log(data);
+          }
+        }
+      }
+      navigateTo("/notes");
+    } catch (error) {}
+  } else {
+    msg.value = "Title or Description can't be blank";
+    isErr.value = true;
+    return;
+  }
+  isErr.value = false;
+};
+</script>
+
+<style lang="scss" scoped></style>
